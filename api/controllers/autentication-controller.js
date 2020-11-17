e056b78d2798bcfd46bd0ce6e40c4cc3cdcc3a90
Junior Andrade <junior.andrade@triasoftware.com.br>
2020-11-17 20:03:23 -0300

    auth

--- api/controllers/autentication-controller.js
+++ api/controllers/autentication-controller.js
@@ -4,7 +4,6 @@ const util = require('util');
 const mongo = require('mongodb').MongoClient
 const url = "mongodb+srv://classificae-user:Junior381414@cluster0.ahqaj.mongodb.net/classificae?retryWrites=true&w=majority";
 
-
 function verifyJWT(req, res, next) {
     var token = req.headers['x-access-token'];
     if (!token) return res.status(401).send({ auth: false, message: 'No token provided.' });
@@ -25,26 +24,27 @@ controller.login = (req, res) => {
         mongo.connect(url, {
             useNewUrlParser: true,
             useUnifiedTopology: true
-          }, (err, client) => {
-          if (err) {
-            console.error(err)
-            return
-          } else {
-            const db = client.db('classificae')
-            const collection = db.collection('company')
-            collection.find({'contact.email': req.body.email, 'password': req.body.password}).toArray((err, items) => {
-                if (items.length > 0) {
-                    let id = items[0]['_id'];
-                    let token = jwt.sign({ id }, 'CJ', {
-                        expiresIn: 300 // expires in 5min
-                    });
-    
-                    res.status(200).send({ auth: true, token: token, user: { id: id, name: items[0].name, logo: items[0].image_logo } });
-                } else {
-                    res.status(401).send({ auth: false, message: 'E-mail e/ou senha estão incorretos'});
-                }
-            });
-          }  
+        }, (err, client) => {
+            if (err) {
+                console.error(err)
+                return
+            } else {
+                const db = client.db('classificae')
+                const collection = db.collection('company')
+                collection.find({ 'contact.email': req.body.email, 'password': req.body.password }).toArray((err, items) => {
+                    if (items.length > 0) {                        
+                        let id = items[0]._id;
+
+                        let token = jwt.sign({ id }, 'CJ', {
+                            expiresIn: 300 // expires in 5min
+                        });
+
+                        res.status(200).send({ auth: true, token: token, user: { id: id, name: items[0].name, logo: items[0].image_logo } });
+                    } else {
+                        res.status(401).send({ auth: false, message: 'E-mail e/ou senha estão incorretos' });
+                    }
+                });
+            }
         })
     } catch (error) {
         console.log(error);
