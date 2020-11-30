88578d83a512e9b2473b02a612535468e7614b61
Geraldo Júnior <juninhuu.andrade@gmail.com>
2020-11-29 21:15:14 -0300

    list active

--- api/controllers/company-controller.js
+++ api/controllers/company-controller.js
@@ -29,7 +29,7 @@ controller.listCompanys = (req, res, next) => {
           } else {
             const db = client.db('classificae');
             const collection = db.collection('company');
-            collection.find({}, { projection: { password: 0 } }).toArray((err, items) => {
+            collection.find({"active": true}, { projection: { password: 0} }).toArray((err, items) => {
                 res.status(200).json({'company': items});
               });
           }  
